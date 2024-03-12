6740a99a3ba21d68ea2146e4d388b51b38116889
Vincenzo Melillo <137229406+Vince-mel@users.noreply.github.com>
2024-03-12 22:23:26 +0100

    .

--- src/sections/Project.tsx
+++ src/sections/Project.tsx
@@ -4,7 +4,8 @@ function Project() {
 
     const Projects = [
         {
-          image:"",
+          image:"/public/theway.png",
+          image2:"/public/thway2.png",
           projectName: "The Waystone",
           Description: "Web-App-blog con login e utenti Admin, User e dashboard di gestione. Permette di scrivere articoli e commentarli, oltre ad avere profili utenti",
           Tech: [
@@ -21,6 +22,64 @@ function Project() {
             demo: "",
           }
         },
+
+        {
+            image:"/public/spotify.jpeg",
+          projectName: "Spotify clone",
+            Description: "Un piccolo esempio di clone della UI di Spotify con operazioni CRUD",
+            Tech: [
+              "React",
+              "Redux",
+              "Javascript",
+             
+            ],
+            Links: {
+              github: "https://github.com/Vince-mel/spotify-react-redux",
+              demo: "https://vincefy.vercel.app/",
+            }
+          },
+
+
+          {
+            image:"/public/theway.png",
+            projectName: "Netflix clone UI",
+            Description: "Un piccolo esempio di clone della UI di netflix con operazioni CRUD",
+            Tech: [
+              "React",
+              "Redux",
+              "Javascript",
+           
+            ],
+            Links: {
+              github: "https://github.com/Vince-mel/netflix-clone-react",
+              demo: "netflix-clone-react-livid.vercel.app",
+            }
+          },
+
+          {
+            image:"/public/Meteoday.png",
+            projectName: "Meteoday",
+            Description: "Piccola web-app Meteo per vedere le previsioni del tempo tramite posizione o ricerca, mostra anche il resto dei giorni della settimana. La UI è da rimodellare, l'esercizio si basava sull'utilizzo di vere API meteo",
+            Tech: [
+              "React",
+              "Redux",
+              "Javascript",
+            
+            ],
+            Links: {
+              github: "https://github.com/Vince-mel/weather-react-app",
+              demo: "weather-react-app-mocha.vercel.app",
+            }
+          },
+
+
+
+
+
+
+
+
+
     ],
 
 
